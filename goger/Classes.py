6b77b13e56a3d364ffda2ca30c00f987348ad021
Lev <53696120+lev-l@users.noreply.github.com>
2020-08-19 08:23:48 +0300

    0.5

--- goger/Classes.py
+++ goger/Classes.py
@@ -37,6 +37,7 @@ class Movenment(Alive):
 
 
 class Button(Alive):
+	"""Class for buttons in menu"""
 	
 	def __init__(self, x, y, scin, width, height):
 		super().__init__(x, y, scin)
@@ -44,6 +45,7 @@ class Button(Alive):
 		self.height = height
 	
 	def pressed(self, mouse_x, mouse_y):
+		"""call when we need to know does the button pressed"""
 		if self.x + self.width > mouse_x > self.x \
 			and self.y + self.height > mouse_y > self.y:
 			return True
@@ -76,11 +78,16 @@ class Player(Movenment):
 	def __init__(self, x, y, hp, atk, scin, profil, weapon, throw, armour, inventory = [], curent_loc = None):
 		#player's inventort
 		self.inventory = inventory
+		#this scin we use when player go down
 		self.standart_scin = scin
+		#and this scin we use when player go left, right or up
 		self.profil = profil
+		#spear
 		self.weapon = weapon
+		#throwen spear
 		self.throw = throw
 		self.armour = armour
+		#only player know in which location he is
 		self.curent_loc = curent_loc
 		super().__init__(x, y, hp, atk, scin)
 
@@ -98,41 +105,31 @@ class Player(Movenment):
 			self.x -= 5
 		elif e[pygame.K_d]:
 			self.scin = self.profil
-			#mind the previos x and y
 			self.prex = self.x
 			self.prey = self.y
-			# course of player's go
 			self.x += 5
 		elif e[pygame.K_w]:
 			self.scin = self.profil
-			#mind the previos x and y
 			self.prex = self.x
 			self.prey = self.y
-			# course of player's go
 			self.y -= 5
 		elif e[pygame.K_s]:
 			self.scin = self.standart_scin
-			#mind the previos x and y
 			self.prex = self.x
 			self.prey = self.y
-			# course of player's go
 			self.y += 5
 		else:
 			pass
 	
 	def loc_end(self):
-		# player must not go away from the screen
+		# the player says which edge of the screen he wants to go beyond
 		if self.x <= 0:
-			#self.x += 5
 			return "<x"
 		if self.y <= 0:
-			#self.y += 5
 			return "<y"
 		if self.x >= HEIGHT - PSize / 2:
-			#self.x -= 5
 			return "x>"
 		if self.y >= WIDTH - PSize / 2:
-			#self.y -= 5
 			return "y>"
 
 	def stoping(self, stop):
@@ -174,17 +171,21 @@ class Spear(Alive):
 		return 0
 
 	def put_to_inventory(self, player):
+		# append title of spear to player's inventory
 		if self.title not in player.inventory and self.damage > player.weapon.damage:
 			player.inventory.append(self.title)
 
 	def put_to_weapons(self, player, thing):
+		# now player can atack by this spear
 		if thing is not player.weapon and self.damage > player.weapon.damage:
 			player.weapon = thing
 	
 	def put_to_armours(self, player, thing):
+		# spear isn't armour
 		pass
 	
 	def put_to_throw(self, player, thing):
+		# spear isn't throwen spear
 		pass
 	
 	def repaint(self):
@@ -198,12 +199,15 @@ class ThrowenSpear(Spear):
 		super().__init__(x, y, damage, scin, title, active)
 	
 	def put_to_weapons(self, player, thing):
+		# throwen spear isn't spear
 		pass
 	
 	def put_to_throw(self, player, thing):
+		# becouse we can have several throwen spear at once
 		player.throw.append(thing)
 	
 	def fly(self, px, py):
+		"""call everytime when spear fly foward"""
 		if self.swing == 0:
 			self.x = px
 			self.y = py
@@ -236,10 +240,10 @@ class Enemy(Movenment):
 	"""class what create evil"""
 
 	def __init__(self, x, y, hp, atk, scin, profil):
-		#i can not explain it on English
-		#переменная которая означает, какой сейчас ход у монстра, и следовательно
-		#куда его нужно сдвинуть
-		self.hod = 0
+		# a variable that means what is the current turn of the monster,
+		# and therefore where it needs to be moved
+		self.turn = 0
+		# enemy can punch player only once
 		self.one_punch = False
 		self.profil = profil
 		self.standart_scin = scin
@@ -248,9 +252,11 @@ class Enemy(Movenment):
 	def kill(self, x, y):
 		global PSize, SSize
 
-		#logic of killing player
+		# logic of killing player
 		if x + PSize + 1 > self.x and y + PSize + 1 > self.y and x < self.x + PSize + 1 and y < self.y + PSize + 1 and self.hp > 0:
 			if self.one_punch == False:
+				# if the enemy touches the player, then until he comes off,
+				# the enemy will not be able to inflict damage a second time.
 				self.one_punch = True
 				return self.atk
 			else:
@@ -261,48 +267,48 @@ class Enemy(Movenment):
 		#-----------------------
 
 	def move(self):
-		#'which "hod" is now?' and move the Enemy
+		#'which turn is now?' and move the Enemy
 		if self.hp > 0:
-			if self.hod == 0:
+			if self.turn == 0:
 				self.scin = self.standart_scin
 				self.y += 5
-			if self.hod == 1:
+			if self.turn == 1:
 				self.y += 5
-			if self.hod == 2:
+			if self.turn == 2:
 				self.y += 5
-			if self.hod == 3:
+			if self.turn == 3:
 				self.y += 5
-			if self.hod == 4:
+			if self.turn == 4:
 				self.scin = self.profil
 				self.x += 5
-			if self.hod == 5:
+			if self.turn == 5:
 				self.x += 5
-			if self.hod == 6:
+			if self.turn == 6:
 				self.x += 5
-			if self.hod == 7:
+			if self.turn == 7:
 				self.x += 5
-			if self.hod == 8:
+			if self.turn == 8:
 				self.y -= 5
-			if self.hod == 9:
+			if self.turn == 9:
 				self.y -= 5
-			if self.hod == 10:
+			if self.turn == 10:
 				self.y -= 5
-			if self.hod == 11:
+			if self.turn == 11:
 				self.y -= 5
-			if self.hod == 12:
+			if self.turn == 12:
 				self.x -= 5
-			if self.hod == 13:
+			if self.turn == 13:
 				self.x -= 5
-			if self.hod == 14:
+			if self.turn == 14:
 				self.x -= 5
-			if self.hod == 15:
+			if self.turn == 15:
 				self.x -= 5
 		#--------------------------
-		#next "hod"
-		self.hod += 1
-		#hod must not be biger then 15
-		if self.hod > 15:
-			self.hod = 0
+		#next turn
+		self.turn += 1
+		#turn must not be biger then 15
+		if self.turn > 15:
+			self.turn = 0
 
 	def repaint(self):
 		global PSize, gog
@@ -319,6 +325,7 @@ class Key(object):
 			player.inventory.append(self.title)
 
 	def put_to_weapons(self, player, thing):
+		# ker isn't spear
 		pass
 	
 	def put_to_throw(self, player, thing):
@@ -345,6 +352,7 @@ class Armour(Key):
 		if thing is not player.armour and self.arm > player.armour.arm:
 			player.armour = thing
 			
+			# visual the armour on the player
 			if "leather" in thing.title:
 				player.standart_scin = pygame.image.load("docs/player_leather.jpg")
 				player.profil = pygame.image.load("docs/player-profil_leather.jpg")
@@ -379,10 +387,11 @@ class Chest(Stop):
 
 		if x + PSize > self.x and y + PSize > self.y and x < self.x + PSize and y < self.y + PSize:
 			#if player had stoped he take the inventory of this chest
-			return "stop"#"stop" - это то что означает, что игрок должен остановиться(sorry for rusian)
+			return "stop"#"stop" - this is what means the player should stop(for refactoring: use 'True' instead 'stop')
 		return "nonstop"
 
 	def empty(self):
+		# clear the chest
 		self.invent = None	
 	
 	def repaint(self):
